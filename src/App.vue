6a0afcd7487f80f13032f406c92282ce9bea574e
kungfuboy <17kungfuboy@gmail.com>
2017-09-28 23:08:43 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="app">
+        <span @click="dialogFormVisible = true">点击出现弹窗</span>
+        
+        <demo :dialogFormVisible.sync="dialogFormVisible"></demo>
+    </div>
+</template>
+
+<script>
+import Demo from './Demo.vue'
+export default {
+    name: 'app',
+    data() {
+        return {
+            dialogFormVisible: false
+        }
+    },
+    components: {
+        Demo
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+h1,
+h2 {
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
---
+++ src/Demo.vue
@@ -0,0 +1,25 @@
+<template>
+    <el-dialog :visible.sync="dialogFormVisible">
+        <div>
+            <el-button @click="changeState">关闭</el-button>
+        </div>
+    </el-dialog>
+</template>
+<script>
+    export default {
+        props: ['dialogFormVisible'],
+        data() {
+            return {
+                dialogState: false
+            }
+        },
+        methods: {
+            changeState() {
+                this.$emit('update:dialogFormVisible', !this.dialogFormVisible)
+            }
+        },
+        mounted() {
+            this.dialogState = this.dialogFormVisible
+        }
+    }
+</script>
